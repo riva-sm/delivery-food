4fdafb524fc3d44635d1217ff281dd527386bd09
Xenia Filippova <32355526+riva-sm@users.noreply.github.com>
2020-05-05 23:24:30 +0800

    updateJs-1

--- js/main.js
+++ js/main.js
@@ -10,3 +10,85 @@ function toggleModal() {
 }
 
 new WOW().init();
+
+//
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+let login = localStorage.getItem('gloDelivery');
+
+function toggleModalAuth() {
+    modalAuth.classList.toggle('is-open');
+}
+
+
+
+function authorized() {
+
+    function logOut() {
+        login = null;
+        localStorage.removeItem('gloDelivery');
+
+
+        buttonAuth.style.display = '';
+        userName.style.display = '';
+        buttonOut.style.display = '';
+        buttonOut.removeEventListener('click', logOut);
+
+        checkAuth();
+
+    };
+
+    console.log('Авторизован');
+
+    userName.textContent = login;
+
+    buttonAuth.style.display = 'none';
+    userName.style.display = 'inline';
+    buttonOut.style.display = 'block';
+
+    buttonOut.addEventListener('click', logOut);
+
+};
+
+function notAuthorized() {
+    console.log('Не авторизован');
+
+    function logIn(event) {
+        event.preventDefault(); // отменяем перезагрузку страницы
+        login = loginInput.value; // получаем введенный логин и сохраняем в перменную
+
+        localStorage.setItem('gloDelivery', login);
+
+        toggleModalAuth();
+        buttonAuth.removeEventListener('click', toggleModalAuth);
+        closeAuth.removeEventListener('click', toggleModalAuth);
+        logInForm.removeEventListener('submit', logIn);
+        logInForm.reset(); // очищаем поля ввода
+        checkAuth(); // проверка авторизации пользователя
+
+
+
+    };
+
+    buttonAuth.addEventListener('click', toggleModalAuth);
+    closeAuth.addEventListener('click', toggleModalAuth);
+    logInForm.addEventListener('submit', logIn);
+
+};
+
+function checkAuth() {
+    if (login) {
+        authorized();
+    } else {
+        notAuthorized();
+    }
+};
+
+checkAuth();
